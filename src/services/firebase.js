dc11cf8955ac2e8a2c63a14e2485a8f48db28e1c
marcospss <marcospss@gmail.com>
2020-06-24 20:52:51 -0300

    Firebase: import only auth

--- src/services/firebase.js
+++ src/services/firebase.js
@@ -1,4 +1,5 @@
-import * as firebase from "firebase";
+import * as firebase from "firebase/app";
+import "firebase/auth";
 import firebaseOptions from "@/settings/firebase";
 import { axiosFirebase } from "@/settings/axios";
 // https://firebase.google.com/docs/database/security
